cb4efbe43e750d80818fd6023674dcddc142d22a
Akshay Daftardar <akshaydaf@gmail.com>
2025-04-17 21:19:51 -0400

    create scaffolding and training scripts

---
+++ train.py
@@ -0,0 +1,35 @@
+from models.vanilla_nn import TwoLayerNet
+from trainer import Trainer
+from trainer import Trainer
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+import argparse
+import yaml
+from config import Config
+import pandas as pd
+
+parser = argparse.ArgumentParser(description="trainer")
+parser.add_argument('--config_file', type=str, default='configs/config_nn.yaml', help="path to YAML config")
+parser.add_argument('--output_dir', type=str, default=None,
+                    help="path to output directory (optional); defaults to outputs/model_name")
+args = parser.parse_args()
+
+with open(args.config_file, 'r') as file:
+    config_dict = yaml.safe_load(file)
+    config = Config(config_dict=config_dict)
+df = pd.read_csv('data/dataset.csv')
+target_column = 'rating'
+sparse_columns = list(set(df.columns) - {'rating'})
+## Do a train test split here
+X_sparse = torch.tensor(df[sparse_columns].values, dtype=torch.float32)
+y = torch.tensor(df[target_column].values, dtype=torch.float32)
+
+dataset = TensorDataset(X_sparse, y)
+loader = DataLoader(dataset, batch_size=config.train.batch_size, shuffle=True)
+
+model = TwoLayerNet(input_dim=len(sparse_columns), hidden_size=784, num_classes=1)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=float(config.train.lr))
+trainer = Trainer(model, optimizer, config, loader)
+
+trainer.fit()
---
+++ trainer.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
+from data_utils.data_utils import set_seed
+
+
+class Trainer:
+    def __init__(self, model, optimizer, cfg, train_loader, val_loader=None):
+        self.model = model.to(cfg.train.device)
+        self.optimizer = optimizer
+        self.cfg = cfg
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        set_seed(seed=42)
+
+    def train_epoch(self):
+        self.model.train()
+        total_loss = 0
+        for sparse, target in tqdm(self.train_loader):
+            sparse = sparse.to(self.cfg.train.device)
+            target = target.to(self.cfg.train.device)
+
+            pred = self.model(sparse)
+            loss = F.binary_cross_entropy(pred, target)
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            total_loss += loss.item()
+        return total_loss / len(self.train_loader)
+
+    def evaluate(self):
+        self.model.eval()
+        total_loss = 0
+        with torch.no_grad():
+            for sparse, target in tqdm(self.train_loader):
+                sparse = sparse.to(self.cfg.train.device)
+                target = target.to(self.cfg.train.device)
+
+                pred = self.model(sparse)
+                loss = F.binary_cross_entropy(pred, target)
+                total_loss += loss.item()
+        return total_loss / len(self.val_loader)
+
+    def fit(self):
+        for epoch in range(self.cfg.train.num_epochs):
+            train_loss = self.train_epoch()
+            print(f"[Epoch {epoch + 1}] Train Loss: {train_loss:.4f}")
+
+            if self.val_loader:
+                val_loss = self.evaluate()
+                print(f"[Epoch {epoch + 1}] Val Loss: {val_loss:.4f}")
